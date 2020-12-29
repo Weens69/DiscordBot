e09844ce5d2bd246df09a5d20a7758459f32027c
Weens69 <izzyraingaming@gmail.com>
2020-12-29 00:58:17 -0500

    asd

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const Discord = require('discord.js');
-const antispam = require('better-discord-antispam');
 const bot = new Discord.Client();
 
 
@@ -146,31 +145,6 @@ bot.on('message', msg=>{
 		msg.react('🇾')
 	}
 })
-
-bot.on('ready', () => {
-	 antispam(bot, {
-		  limitUntilWarn: Integer, // The amount of messages allowed to send within the interval(time) before getting a warn.
-		  limitUntilMuted: Integer, // The amount of messages allowed to send within the interval(time) before getting a muted.
-		  interval: 2000, // The interval(time) where the messages are sent. Practically if member X sent 5+ messages within 2 seconds, he get muted. (1000 milliseconds = 1 second, 2000 milliseconds = 2 seconds etc etc)
-		  warningMessage: "", // Message you get when you are warned!
-		  muteMessage: "", // Message sent after member X was punished(muted).
-		  maxDuplicatesWarning: 7,// When people are spamming the same message, this will trigger when member X sent over 7+ messages.
-		  maxDuplicatesMute: 10, // The limit where member X get muted after sending too many messages(10+).
-		  ignoredRoles: Array, // The members with this role(or roles) will be ignored if they have it. Suggest to not add this to any random guys. Also it's case sensitive.
-		  ignoredMembers: Array, // These members are directly affected and they do not require to have the role above. Good for undercover pranks.
-		  mutedRole:  "muted", // Here you put the name of the role that should not let people write/speak or anything else in your server. If there is no role set, by default, the module will attempt to create the role for you & set it correctly for every channel in your server. It will be named "muted".
-		  timeMuted: 1000 * 600, // This is how much time member X will be muted. if not set, default would be 10 min.
-		  logChannel:  String // This is the channel where every report about spamming goes to. If it's not set up, it will attempt to create the channel.
-		});
-
-	});		
-
-	bot.on('message', msg => {
-		bot.emit('checkMessage', msg);
-
-
-	})		
-
 
 
 
