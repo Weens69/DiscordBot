333914fd00be3d448b72430fd4499633c2bf98a7
Weens69 <izzyraingaming@gmail.com>
2020-12-29 00:41:14 -0500

    anti

--- index.js
+++ index.js
@@ -149,7 +149,15 @@ bot.on('message', msg=>{
         limitUntilWarn: 3,
         limitUntilMuted: 5,
         interval: 2000,
+        warningMessage: "",
+        muteMessage: "",
+        maxDuplicatesWarning: 7,
+        maxDuplicatesMute: 10,
+        ignoredRoles: [],
+        ignoredMembers: [],
+		mutedRole:"",
 		timeMuted: 1000*600,
+		logChannel: ""
       });
 })
 
