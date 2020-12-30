c31905771ed24a5aa27b10736e3fdcdaaa45d3b4
Weens69 <izzyraingaming@gmail.com>
2020-12-29 21:26:27 -0500

    poops

--- index.js
+++ index.js
@@ -135,7 +135,7 @@ bot.on('message', msg=>{
 	if(msg.author.id === '783932326366150668'){
 		msg.react('<:pepeLaugh:790919170169634857>');
 		msg.react('👉')
-		msg.react('🥚')
+		msg.react('💩')
 	}
 	if(msg.author.id === '758400928591118378'){
 		msg.react('<:pepeLaugh:790919170169634857>');
